39806d8c3d80098f2f623b02a724388f9e875334
Daniil Tsitavets <d.tsitavets@gmail.com>
2024-10-12 19:24:57 +0300

    feature: add method

--- src/main/java/com/dt/manager/repository/PasswordRepository.java
+++ src/main/java/com/dt/manager/repository/PasswordRepository.java
@@ -4,6 +4,10 @@ import com.dt.manager.entity.Password;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PasswordRepository extends JpaRepository<Password, Integer> {
+    Optional<Password> findByServiceName(String serviceName);
+
 }
