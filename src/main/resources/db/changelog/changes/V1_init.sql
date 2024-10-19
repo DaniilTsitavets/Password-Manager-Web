25723bf9b4588ec53b977e0d2454e09531216f94
Daniil Tsitavets <d.tsitavets@gmail.com>
2024-10-19 15:26:52 +0300

    refactor: rename

--- src/main/resources/db/changelog/changes/V1_init.sql
+++ src/main/resources/db/changelog/changes/V1_init.sql
@@ -2,7 +2,7 @@ CREATE TABLE "password"(
                            "id" bigint NOT NULL,
                            "password_value" VARCHAR(255) NOT NULL,
                            "service_name" VARCHAR(255) NOT NULL,
-                           "date" DATE NOT NULL
+                           "createdAt" DATE NOT NULL
 );
 ALTER TABLE
     "password" ADD PRIMARY KEY("id");
